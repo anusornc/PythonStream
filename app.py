4908afa4299569202459531c00c0d6a16f019e38
Pathiphon <611463010@crru.ac.th>
2021-02-03 13:48:04 +0700

    send work

--- app.py
+++ app.py
@@ -3,3 +3,25 @@ import cv2
 
 app = Flask(__name__)
 
+camera = cv2.VideoCapture(0) #0 คือ index อุปกรณ์เว็บแคป
+
+def gen_frames():
+    while True: #จะหยุดต้องstop ที่ server 
+        success,frame=camera.read()
+        if not success:
+            break
+        else:
+            ret,buffer = cv2.imencode('.jpg',frame)
+            frame = buffer.tobytes()
+            yield(b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n'+frame+b'\r\n') #ผลิตไรซักอย่าง
+@app.route("/video_feed")
+def video_feed():
+    return Response(gen_frames(),mimetype='multipart/x-mixed-replace; boundary=frame')
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__=='__main__':
+    app.run(debug=True)
+
